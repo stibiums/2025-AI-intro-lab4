19ccf4bfdf2eab98d28259c2faa8424d2918d7b5
stibiums <1360023994@qq.com>
2025-06-11 16:57:33 +0800

    Optuna Trial 15: Score=9.21, Params=(W=0.236,ALPHA=1.500,sigma_P=0.038,sigma_T=0.021,K=4)

--- answerLocalization.py
+++ answerLocalization.py
@@ -6,11 +6,11 @@ from utils import Particle
 COLLISION_DISTANCE = 1
 MAX_ERROR = 50000
 
-W = 0.2590   # 权重常数 (auto-tuned)
-ALPHA = 1.0004  # 每个点重采样比占比例多的倍数 (auto-tuned)
-sigma_P = 0.1560  # 位置方差 (auto-tuned)
-sigma_T = 0.0493  # 角度方差 (auto-tuned)
-K = 1  # 采样点数量 (auto-tuned)
+W = 0.2363   # 权重常数 (auto-tuned)
+ALPHA = 1.4995  # 每个点重采样比占比例多的倍数 (auto-tuned)
+sigma_P = 0.0383  # 位置方差 (auto-tuned)
+sigma_T = 0.0205  # 角度方差 (auto-tuned)
+K = 4  # 采样点数量 (auto-tuned)
 ### 可以在这里写下一些你需要的变量和函数 ###
 
 
