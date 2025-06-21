027701c11e3fc501ea9f1526a1c48f60b076c4d1
stibiums <1360023994@qq.com>
2025-06-21 20:06:05 +0800

    Optuna Planning Trial 211: Score=94.27, Params=(STEP_DISTANCE=0.807,GOAL_BIAS=0.161)

--- answerPlanning.py
+++ answerPlanning.py
@@ -5,10 +5,10 @@ from simuScene import PlanningMap
 
 
 ### 定义一些你需要的变量和函数 ###
-STEP_DISTANCE = 0.8145         # RRT 每次扩展的步长 (auto-tuned)
+STEP_DISTANCE = 0.8073         # RRT 每次扩展的步长 (auto-tuned)
 TARGET_THREHOLD = 0.25      # 到达目标的距离阈值
 MAX_ITER = 2500             # RRT 的最大迭代次数 (auto-tunable)
-GOAL_BIAS = 0.1650             # RRT 采样时朝向目标的概率 (auto-tuned)
+GOAL_BIAS = 0.1608             # RRT 采样时朝向目标的概率 (auto-tuned)
 ### 定义一些你需要的变量和函数 ###
 
 
